dae190709d322cebecfc86ffb8f973888c5f7699
VAGalstyan <vahe0899@mail.ru>
2022-12-29 18:19:38 +0700

    Реализовал работу конвертера

--- src/Components/Exchange/Exchange.jsx
+++ src/Components/Exchange/Exchange.jsx
@@ -1,8 +1,9 @@
 import '../Exchange/_Exchange.css'
 import { useDispatch, useSelector } from 'react-redux';
-import { refresh, valutesLoad } from '../../Redux/actions';
+import { refresh, valutesLoad, setMainValute, setConvertedValute, convertedInput, mainInput } from '../../Redux/actions';
 import { useEffect } from 'react';
 import TableRow from '../TableRow/TableRow';
+import SelectItem from '../SelectItem/SelectItem';
 
 function Exchange() {
     const dispatch = useDispatch();
@@ -10,6 +11,18 @@ function Exchange() {
     const valute = useSelector(state => {
         return state.valute
     });
+
+    const mainInputValue = useSelector(state => {
+        return state.mainInputValue
+    });
+
+    const convertedInputValue = useSelector(state => {
+        return state.convertedInputValue
+    });
+
+    // const mainValute = useSelector(state => {
+    //     return state.convertedValute
+    // });
 
     const euro = useSelector(state => {
         return state.valute.find(item => item.CharCode == 'EUR')
@@ -23,13 +36,29 @@ function Exchange() {
         return state.valute.find(item => item.CharCode == 'CNY')
     });
 
-    const refreshHandler = () => {
-        dispatch(valutesLoad());
-    };  
+    // const refreshHandler = () => {
+    //     dispatch(valutesLoad());
+    // };  
+
+    const mainValuteHandler = (event) => {
+        dispatch(setMainValute(event.target.value));
+    }; 
+
+    const convertedValuteHandler = (event) => {
+        dispatch(setConvertedValute(event.target.value));
+    };
+
+    const mainInputHandler = (event) => {
+        dispatch(mainInput(event.target.value));
+    };
+
+    const convertedInputHandler = (event) => {
+        dispatch(convertedInput(event.target.value));
+    };
 
     useEffect(() => {
         dispatch(valutesLoad())
-    }, [1]);
+    }, []);
 
     return(
     <div className="container">
@@ -41,7 +70,7 @@ function Exchange() {
                 <div className="time">
                     Обновлено в последний раз: СЕЙЧАС
                 </div>
-                <button className="refresh" onClick={refreshHandler}>
+                <button className="refresh">
                     Обновить
                 </button>
             </div>
@@ -61,16 +90,21 @@ function Exchange() {
             </tbody>
         </table>
         <div className="converter">
-            <div className="inputs">
-                <input placeholder='INPUT'></input>
-                <input placeholder='INPUT'></input>
-            </div>
-            <div className="dropdowns">
-                <input placeholder='DROPDOWN'></input>
-                <input placeholder='DROPDOWN'></input>
-            </div>
+                <div className="dropdown">
+                    <select className="dropbtn" onChange={mainValuteHandler}>
+                        <option disabled selected value> -- Выберите валюту -- </option>
+                        {valute.map((item) => <SelectItem data={item} id={valute.indexOf(item) + 1}/>)}
+                    </select>   
+                    <input type="number" placeholder='Введите значение...' onChange={mainInputHandler} value={mainInputValue}></input>
+                </div>    
+                <div className="dropdown">
+                    <input type="number" placeholder='Введите значение...' onChange={convertedInputHandler} value={convertedInputValue}></input>
+                    <select className="dropbtn" onChange={convertedValuteHandler}>
+                        <option disabled selected value> -- Выберите валюту -- </option>
+                        {valute.map((item) => <SelectItem data={item} id={valute.indexOf(item) + 1}/>)}
+                    </select>   
+                </div>     
         </div>
-
     </div>
     )
 }
